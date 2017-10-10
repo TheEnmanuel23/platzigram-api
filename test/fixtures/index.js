dc573236c4d50dba2c7fac05e380fb51dd028063
enmisac <enmisac@gmail.com>
2017-10-10 00:03:31 -0600

    se implemento la ruta de likes

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -4,6 +4,8 @@ export default {
       id: '02b353bf5358995bc7d193ed1ce9c2eaec2b694b21d2f96232c9d6a0832121d1',
       publicId: '2X9OfsAGDVMIIrLVgkqcuF',
       userId: 'platzigram',
+      likes: 0,
+      liked: false,
       src: 'http://platzigram/2X9OfsAGDVMIIrLVgkqcuF.jpg',
       description: '#awesome',
       tags: [ 'awesome' ],
