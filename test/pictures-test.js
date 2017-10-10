855ec11937ccede737523f0106adb47b52ac1d9b
enmisac <enmisac@gmail.com>
2017-10-09 21:17:58 -0600

    logre hacer la primer ruta y configuracion general de http-hasgh con micro

---
+++ test/pictures-test.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import test from 'ava'
+import micro from 'micro'
+import uuid from 'uuid-base62'
+import listen from 'test-listen'
+import request from 'request-promise'
+import pictures from '../pictures'
+
+test('GET /:id', async t => {
+  let id = uuid.v4()
+
+  let srv = micro(pictures)
+
+  let url = await listen(srv)
+  let body = await request({ uri: `${url}/${id}`, json: true })
+  console.log('>>>>', body)
+  console.log('*****', id)
+  t.deepEqual(body, { id })
+})
+
+test.todo('POST /')
+test.todo('')
