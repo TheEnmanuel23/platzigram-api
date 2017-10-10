84fb17ef076820f33f4153b81a9195a9eff3dc59
enmisac <enmisac@gmail.com>
2017-10-09 22:12:57 -0600

    se agregaron stubs y cosas el test con un objeto image completo

---
+++ test/fixtures/index.js
@@ -0,0 +1,13 @@
+export default {
+  getImage () {
+    return {
+      id: '02b353bf5358995bc7d193ed1ce9c2eaec2b694b21d2f96232c9d6a0832121d1',
+      publicId: '2X9OfsAGDVMIIrLVgkqcuF',
+      userId: 'platzigram',
+      src: 'http://platzigram/2X9OfsAGDVMIIrLVgkqcuF.jpg',
+      description: '#awesome',
+      tags: [ 'awesome' ],
+      createAt: new Date().toString()
+    }
+  }
+}
